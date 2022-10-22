ef70e68f9fb82250b91914f0d7e1e545a6f61f5f
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-23 00:58:58 +0600

    Learn How Default layout work

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,5 @@
 <template>  
     <div>
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="about">About</NuxtLink>
         <h1>About</h1>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="about">About</NuxtLink>
     <h1>Home</h1>
   </div>
 </template>
