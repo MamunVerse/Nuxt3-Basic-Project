c7f859f3209b7113f235e1c7f21a0d9e3e19a36b
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-23 00:00:23 +0600

    Make Counter Component

--- app.vue
+++ app.vue
@@ -1,22 +1,5 @@
 <template>
-  <div>
-    <h1>Hello Mamun</h1>
-    <button @click="increment">Increment</button>
-    <p>{{ count }}</p>
-    <button @click="decrement">Decrement</button>
-  </div>
+  <Counter/>
 </template>
-
-<script setup>
-  const count = ref(0);
-
-  function increment(){
-    count.value++;
-  }
-
-  function decrement(){
-    count.value--;
-  }
-</script>
 
  
---
+++ components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hello Mamun</h1>
+    <button @click="increment">Increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">Decrement</button>
+</template>
+
+<script setup>
+  const count = ref(0);
+
+  function increment(){
+    count.value++;
+  }
+
+  function decrement(){
+    count.value--;
+  }
+</script>
