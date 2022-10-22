fd599399bcc589c4af3f2ae85c961ca514f7344d
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-22 23:58:32 +0600

    Create basic counter

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello Mamun</h1>
+    <button @click="increment">Increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">Decrement</button>
   </div>
 </template>
+
+<script setup>
+  const count = ref(0);
+
+  function increment(){
+    count.value++;
+  }
+
+  function decrement(){
+    count.value--;
+  }
+</script>
+
+ 
