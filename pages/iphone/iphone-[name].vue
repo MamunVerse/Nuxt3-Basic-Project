b1421ac1615aed1e1380784ffef250ad1a3918c0
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-23 18:10:04 +0600

    Learn Dynamic directory

---
+++ pages/iphone/iphone-[name].vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Iphone 14 {{ name }}</h1>
+</template>
+
+<script setup>
+
+    const route = useRoute();
+
+    const name = computed(()=>{
+        return route.params.name.replaceAll('-', ' ');
+    })
+</script>
